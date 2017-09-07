b2645a254e3bb66d4eb8154b288d39667424c898
Brandon <brandonroberge@hotmail.ca>
2017-09-07 15:15:59 -0400

    Initial commit

---
+++ auth/Controllers/RootController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureTokenService.Controllers
+{
+    [Route("api")]
+    public class RootController : Controller
+    {
+        // GET /api
+        [HttpGet]
+        public string Get()
+        {
+            return "STS v1.0";
+        }
+    }
+}
