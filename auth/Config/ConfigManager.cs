6241abcce0bdcf137bf45e5378e4f330218e3341
Brandon <brandonroberge@hotmail.ca>
2017-09-09 14:55:06 -0400

    Update

--- auth/Config/ConfigManager.cs
+++ auth/Config/ConfigManager.cs
@@ -3,9 +3,7 @@
     public static class ConfigManager
     {
         // Mongo config
-        public static string DatabaseHost = Environment.Get("DB_HOST");
-        public static string DatabaseName = Environment.Get("DB_NAME");
-        public static string DatabaseUsername = Environment.Get("DB_USERNAME");
-        public static string DatabasePassword = Environment.Get("DB_PASSWORD");
+        public static string DatabaseConnectionString = Environment.Get("DB_CONNECTION_STRING") ?? "";
+        public static string DatabaseName = Environment.Get("DB_NAME") ?? "";
     }
 }
--- auth/Models/Database.cs
+++ auth/Models/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MongoDB.Driver;
+using SecureTokenService.Config;
 using SecureTokenService.Interfaces;
 
 namespace SecureTokenService.Models
@@ -10,13 +11,18 @@ namespace SecureTokenService.Models
 
         public Database()
         {
-            var credentials = $"{Environment.Get("DB_USERNAME")}:{Environment.Get("DB_PASSWORD")}";
-            var connectionString = $"mongodb://{Environment.Get("DB_HOST")}:27017";
-            var client = new MongoClient(connectionString);
-            _db = client.GetDatabase(Environment.Get("DB_NAME"));
-            Console.WriteLine(_db != null
-                ? $"Successfully connected to MongoDB with connection string {connectionString}"
-                : $"Failed to connect to MongoDB with connection string {connectionString}");
+            try
+            {
+                var client = new MongoClient(ConfigManager.DatabaseConnectionString);
+                _db = client.GetDatabase(ConfigManager.DatabaseName);
+                Console.WriteLine(_db != null
+                    ? $"Successfully connected to MongoDB with connection string {ConfigManager.DatabaseConnectionString}"
+                    : $"Failed to connect to MongoDB with connection string {ConfigManager.DatabaseConnectionString}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to connect to MongoDB with connection string {ConfigManager.DatabaseConnectionString}");
+            }
         }
 
         public IMongoCollection<T> GetCollection<T>(string name)
