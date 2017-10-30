491bb3f1efc49ab2bf5744859881c08b22b741a1
JulienDuf <dufresne_julien@hotmail.ca>
2017-10-29 20:02:12 -0400

    Update PermissionGuard

--- api/src/guards/permission.guard.ts
+++ api/src/guards/permission.guard.ts
@@ -4,26 +4,23 @@ import { Reflector } from '@nestjs/core';
 
 @Guard()
 export class PermissionsGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector) {}
+    constructor(private readonly reflector: Reflector) {
+    }
 
     canActivate(req: express.Request, context: ExecutionContext): boolean {
-        const { parent, handler } = context;
-        const permissions = this.reflector.get<string[]>('permissions', handler);
-
-        if (!permissions) {
-            return true;
-        }
-
         if (!req.user) {
             return false;
-        } else {
-            let userPermissions: string[] = JSON.parse(req.user.permissions);
-            for (let p of permissions) {
-                if (!userPermissions.includes(p)) {
-                    return false;
-                }
-            }
-            return true;
         }
+
+        const permissions = this.reflector.get<string[]>('permissions', context.handler);
+
+        let userPermissions: string[] = JSON.parse(req.user.permissions);
+        for (let p of permissions) {
+            if (!userPermissions.includes(p)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
