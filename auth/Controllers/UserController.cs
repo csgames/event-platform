1dc7dc09536410ada2d6b421d8970fb571517e31
Laurent Caron <laurentcarondufour@gmail.com>
2017-11-20 15:08:05 -0500

    Changed controller name and changed update user method (#3)

---
+++ auth/Controllers/UserController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using STS.Attributes;
+using STS.Interface;
+using STS.User;
+
+namespace STS.Controllers
+{
+
+    [Route("user")]
+    public class RegisterController : Controller
+    {
+        private enum ErrorCode
+        {
+            WrongOldPasswordError = 26
+        }
+
+        private readonly IRepository _db;
+
+        public RegisterController(IRepository db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("attendee")]
+        public Task<IActionResult> CreateAttendee(AttendeeRegisterInput input)
+        {
+            return Task.Run<IActionResult>(() =>
+            {
+                if (!ModelState.IsValid)
+                {
+                    Console.WriteLine(input);
+                    return BadRequest();
+                }
+                var user = _db.Single<User.User>(u => u.Username == input.Username);
+                if (user != null)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.Conflict);
+                }
+                try
+                {
+                    var hashedPassword = BCrypt.Net.BCrypt.HashPassword(input.Password);
+                    user = new User.User()
+                    {
+                        Username = input.Username,
+                        Password = hashedPassword,
+                        BirthDate = input.BirthDate,
+                        RoleId = _db.Single<User.Role>(r => r.Name == "attendee").Id,
+                        Email = input.Email,
+                        FirstName = input.FirstName,
+                        LastName = input.LastName
+                    };
+                    _db.Add(user);
+                    return Ok(new
+                    {
+                        success = true,
+                        userId = user.Id
+                    });
+                }
+                catch (Exception)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+                }
+            });
+        }
+
+        [Authorize]
+        [RequiresPermissions("sts:create:user")]
+        [HttpPost]
+        public Task<IActionResult> Post(UserRegisterInput input)
+        {
+            return Task.Run<IActionResult>(() =>
+            {
+                if (!ModelState.IsValid)
+                {
+                    Console.WriteLine(input);
+                    return BadRequest();
+                }
+                var user = _db.Single<User.User>(u => u.Username == input.Username);
+                if (user != null)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.Conflict);
+                }
+                try
+                {
+                    var hashedPassword = BCrypt.Net.BCrypt.HashPassword(input.Password);
+                    user = new User.User()
+                    {
+                        Username = input.Username,
+                        Password = hashedPassword,
+                        RoleId = input.RoleId,
+                        BirthDate = input.BirthDate,
+                        Email = input.Email,
+                        FirstName = input.FirstName,
+                        LastName = input.LastName
+                    };
+                    _db.Add(user);
+                    return Ok(new
+                    {
+                        success = true,
+                        userId = user.Id
+                    });
+                }
+                catch (Exception)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+                }
+            });
+        }
+
+        [Authorize]
+        [RequiresPermissions("sts:update:user")]
+        [HttpPut("{id}")]
+        public Task<IActionResult> UpdateUser(string id, ChangeUserInput input)
+        {
+            return Task.Run<IActionResult>(() =>
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                var user = _db.Single<User.User>(u => u.Id == id);
+                if (user == null)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+                }
+                try
+                {
+                    if (!BCrypt.Net.BCrypt.Verify(input.OldPassword, user.Password))
+                    {
+                        return StatusCode((int)HttpStatusCode.BadRequest, new
+                        {
+                            success = true,
+                            code = ErrorCode.WrongOldPasswordError
+                        });
+                    }
+                    var hashedNewPassword = BCrypt.Net.BCrypt.HashPassword(input.NewPassword);
+                    _db.Update<User.User>(user.Id, new Dictionary<string, object>()
+                    {
+                        {"Password", hashedNewPassword},
+                        {"Username", input.Username},
+                        {"RoleId", input.RoleId},
+                        {"Email", input.Email},
+                        {"FirstName", input.FirstName},
+                        {"LastName", input.LastName},
+                        {"BirthDate", input.BirthDate}
+                    });  
+                    return Ok(new
+                    {
+                        success = true,
+                    });
+                }
+                catch (Exception)
+                {
+                    return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+                }
+            });
+        }
+    }
+}
---
+++ auth/Inputs/ChangeUserInput.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STS.User
+{
+    public class ChangeUserInput
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string RoleId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string BirthDate { get; set; }
+    }
+}
