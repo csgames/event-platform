30f1e84ae87f1c15796820cffe2e5f12b0244f99
Brandon <brandonroberge@hotmail.ca>
2018-01-11 07:48:24 -0500

    Stuff

--- mobile/lib/pages/eventlist.dart
+++ mobile/lib/pages/eventlist.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:PolyHxApp/components/loadingspinner.dart';
 import 'package:PolyHxApp/components/pagetransformer/eventpageitem.dart';
 import 'package:PolyHxApp/components/pagetransformer/pagetransformer.dart';
 import 'package:PolyHxApp/domain/event.dart';
@@ -23,52 +24,51 @@ class EventList extends StatelessWidget {
     return new FutureBuilder(
         future: isLoggedIn(),
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return new Text('Awaiting result...');
-            default:
-              if (!snapshot.hasError && snapshot.data) {
-                return new Scaffold(
-                  appBar: new AppBar(
-                    title: new Text("Events"),
-                    leading: new IconButton(
-                        icon: new Icon(Icons.view_headline),
-                        color: Colors.white,
-                        onPressed: null),
-                  ),
-                  body: new Center(
-                    child: new SizedBox.fromSize(
-                      size: const Size.fromHeight(500.0),
-                      child: new PageTransformer(
-                        pageViewBuilder: (context, visibilityResolver) {
-                          return new PageView.builder(
-                            controller:
-                                new PageController(viewportFraction: 0.85),
-                            itemCount: 2,
-                            itemBuilder: (context, index) {
-                              final item = new Event(
-                                  name: "Hackatown 2018",
-                                  coverUrl: "https://i.imgur.com/VwtbfRu.jpg",
-                                  imageUrl: "https://i.imgur.com/53HXN2a.png");
-                              final pageVisibility = visibilityResolver
-                                  .resolvePageVisibility(index);
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return new Scaffold(body: new Center(child: new LoadingSpinner()));
+          } else {
+            if (!snapshot.hasError && snapshot.data != null && snapshot.data) {
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: new Text("Events"),
+                  leading: new IconButton(
+                      icon: new Icon(Icons.view_headline),
+                      color: Colors.white,
+                      onPressed: null),
+                ),
+                body: new Center(
+                  child: new SizedBox.fromSize(
+                    size: const Size.fromHeight(500.0),
+                    child: new PageTransformer(
+                      pageViewBuilder: (context, visibilityResolver) {
+                        return new PageView.builder(
+                          controller:
+                              new PageController(viewportFraction: 0.85),
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            final item = new Event(
+                                name: "Hackatown 2018",
+                                coverUrl: "https://i.imgur.com/VwtbfRu.jpg",
+                                imageUrl: "https://i.imgur.com/53HXN2a.png");
+                            final pageVisibility =
+                                visibilityResolver.resolvePageVisibility(index);
 
-                              return new EventPageItem(
-                                item: item,
-                                pageVisibility: pageVisibility,
-                              );
-                            },
-                          );
-                        },
-                      ),
+                            return new EventPageItem(
+                              item: item,
+                              pageVisibility: pageVisibility,
+                            );
+                          },
+                        );
+                      },
                     ),
                   ),
-                );
-              } else {
-                new Future.delayed(new Duration(milliseconds: 1),
-                    () => Navigator.of(context).pushReplacementNamed('/login'));
-                return new Container();
-              }
+                ),
+              );
+            } else {
+              new Future.delayed(new Duration(milliseconds: 1),
+                  () => Navigator.of(context).pushReplacementNamed('/login'));
+              return new Container();
+            }
           }
         });
   }
