7c50d61900dbada76c9eda0dd6b46a0113106f45
Rémi Pelletier <remi.pelletier@polymtl.ca>
2018-01-18 14:43:56 -0500

    Added Attendee class

---
+++ mobile/lib/domain/attendee.dart
@@ -0,0 +1,80 @@
+enum Degree { Cegep, Bachelor, Master, PhD }
+
+enum Gender { Male, Female, Other, NoAnswer }
+
+enum ShirtSize { Small, Medium, Large, XLarge, XXLarge, Invalid }
+
+class Attendee {
+  static final Map<String, Degree> _DEGREES = {
+    'cegep': Degree.Cegep,
+    'bachelor': Degree.Bachelor,
+    'master': Degree.Master,
+    'phd': Degree.PhD,
+  };
+
+  static final Map<String, Gender> _GENDERS = {
+    'male': Gender.Male,
+    'female': Gender.Female,
+    'other': Gender.Other,
+    'no_answer': Gender.NoAnswer,
+  };
+
+
+  static final Map<String, ShirtSize> _SHIRT_SIZES = {
+    'small': ShirtSize.Small,
+    'medium': ShirtSize.Medium,
+    'large': ShirtSize.XLarge,
+    'x-large': ShirtSize.XLarge,
+    '2x-large': ShirtSize.XXLarge,
+  };
+
+  String userId;
+  String github;
+  String linkedIn;
+  String cv;
+  String website;
+  Degree degree;
+  Gender gender;
+  ShirtSize shirtSize;
+  String phoneNumber;
+  bool acceptSmsNotification;
+  bool hasDietaryRestrictions;
+  String dietaryRestrictions;
+  String schoolId;
+  String publicId;
+
+  Attendee({
+    this.userId,
+    this.github,
+    this.linkedIn,
+    this.cv,
+    this.website,
+    this.degree,
+    this.gender,
+    this.shirtSize,
+    this.phoneNumber,
+    this.acceptSmsNotification,
+    this.hasDietaryRestrictions,
+    this.dietaryRestrictions,
+    this.schoolId,
+    this.publicId
+  });
+
+  Attendee.fromMap(Map<String, dynamic> map) {
+    userId = map['userId'];
+    github = map['github'];
+    linkedIn = map['linkedIn'];
+    cv = map['cv'];
+    website = map['website'];
+    degree = _DEGREES[map['degree']];
+    gender = _GENDERS[map['gender']];
+    shirtSize = _SHIRT_SIZES[map['tshirt']];
+    phoneNumber = map['phoneNumber'];
+    acceptSmsNotification = map['acceptSMSNotification'];
+    hasDietaryRestrictions = map['hasDietaryRestrictions'];
+    dietaryRestrictions = map['dietaryRestrictions'];
+    schoolId = map['school'];
+    publicId = map['publicId'];
+  }
+}
+
