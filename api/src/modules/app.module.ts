79136d8f5062a4703d2b756b6437df6db8bf5f48
Brandon <brandonroberge@hotmail.ca>
2018-01-17 19:56:51 -0500

    Activity controller

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -5,9 +5,11 @@ import { RegistrationModule } from "./registration/registration.module";
 import { SchoolsModule } from "./database/schools/schools.module";
 import { TeamsModule } from "./database/teams/teams.module";
 import { MailGateway } from "../gateways/mail.gateway";
+import { ActivitiesModule } from './database/activities/activities.module';
 
 @Module({
     modules: [
+        ActivitiesModule,
         AttendeesModule,
         EventsModule,
         RegistrationModule,
