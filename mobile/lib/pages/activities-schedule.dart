4fdea8b28b7f8f573c609abf4acda3eb5e374f7a
Rémi Pelletier <remi.pelletier@polymtl.ca>
2018-01-20 02:58:52 -0500

    Replaced user profile by its component

--- mobile/lib/pages/activities-schedule.dart
+++ mobile/lib/pages/activities-schedule.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-import 'package:PolyHxApp/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/src/material/scaffold.dart';
 import 'package:intl/intl.dart';
 import 'package:PolyHxApp/components/activity-card.dart';
-import 'package:PolyHxApp/components/circle-gravatar.dart';
 import 'package:PolyHxApp/components/loading-spinner.dart';
+import 'package:PolyHxApp/components/user-profile.dart';
 import 'package:PolyHxApp/domain/activity.dart';
 import 'package:PolyHxApp/domain/user.dart';
 import 'package:PolyHxApp/services/attendees.service.dart';
@@ -50,6 +49,17 @@ class _ActivitiesScheduleState extends State<ActivitiesSchedulePage>
     _fetchAllActivities();
   }
 
+  Map<String, List<Activity>> _getActivitiesPerDay(List<Activity> activities) {
+    var formatter = new DateFormat.MMMMd('en_US');
+    Map<String, List<Activity>> dates = {};
+    for (var activity in activities) {
+      var date = formatter.format(activity.beginDate);
+      dates[date] ??= [];
+      dates[date].add(activity);
+    }
+    return dates;
+  }
+
   _fetchAllActivities() async {
     setState(() {
       _isLoading = true;
@@ -62,57 +72,6 @@ class _ActivitiesScheduleState extends State<ActivitiesSchedulePage>
       _activitiesPerDay = activitiesPerDay;
       _isLoading = false;
     });
-  }
-
-  Widget _buildAvatar() {
-    return new Align(
-      alignment: Alignment.topCenter,
-      child: new CircleGravatar(_shownUser.username),
-    );
-  }
-
-  Widget _buildUserDialogBody() {
-    return new Padding(
-      padding: new EdgeInsets.only(top: 40.0),
-      child: new Opacity(
-        opacity: 0.75,
-        child: new Material(
-          elevation: 1.0,
-          borderRadius: new BorderRadius.circular(10.0),
-          child: new Center(
-            child: new Padding(
-              padding: new EdgeInsets.only(top: 60.0),
-              child: new Text('${_shownUser.firstName} ${_shownUser.lastName}',
-                style: new TextStyle(
-                  color: Constants.POLYHX_GREY,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-            ),
-          ),
-        ),
-      )
-    );
-  }
-
-  Widget _buildUserDialog() {
-    if (_shownUser == null) {
-      return new Container();
-    }
-    return new Center(
-      child: new Container(
-        width: 300.0,
-        height: 200.0,
-        child: new Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            _buildUserDialogBody(),
-            _buildAvatar(),
-          ],
-        )
-      ),
-    );
   }
 
   _setCurrentAttendee(BuildContext context, String publicId) async {
@@ -128,57 +87,54 @@ class _ActivitiesScheduleState extends State<ActivitiesSchedulePage>
     if (user == null) {
       return;
     }
-    new Future.delayed(new Duration(seconds: 2), () {
+   /* new Future.delayed(new Duration(seconds: 2), () {
       if (user == _shownUser) {
         _attendeePublicId = null;
         setState(() {
           _shownUser = null;
         });
       }
-    });
+    });*/
     setState(() {
       _shownUser = user;
     });
   }
 
-  Map<String, List<Activity>> _getActivitiesPerDay(List<Activity> activities) {
-    var formatter = new DateFormat.MMMMd('en_US');
-    Map<String, List<Activity>> dates = {};
-    for (var activity in activities) {
-      var date = formatter.format(activity.beginDate);
-      dates[date] ??= [];
-      dates[date].add(activity);
-    }
-    return dates;
-  }
-
-  Widget _buildPage(BuildContext context) {
-    return new Stack(
-      children: <Widget>[
-        new Column(
-          children: <Widget> [
-            new TabBar(
-              labelColor: Colors.black,
-              controller: _tabController,
-              tabs: _activitiesPerDay.keys.map((d) => new Tab(text: d)).toList(),
-            ),
-            new Flexible(
-              child: new TabBarView(
-                controller: _tabController,
-                children: _activitiesPerDay.keys.map((d) =>
-                new SingleChildScrollView(
-                  child: new Column(
-                    children: _activitiesPerDay[d].map((a) =>
-                    new ActivityCard(a)).toList(),
-                  ),
-                ),
-                ).toList(),
+  Widget _buildActivitiesList() {
+    return new Column(
+      children: <Widget> [
+        new TabBar(
+          labelColor: Colors.black,
+          controller: _tabController,
+          tabs: _activitiesPerDay.keys.map((d) => new Tab(text: d)).toList(),
+        ),
+        new Flexible(
+          child: new TabBarView(
+            controller: _tabController,
+            children: _activitiesPerDay.keys.map((d) =>
+            new SingleChildScrollView(
+              child: new Column(
+                children: _activitiesPerDay[d].map((a) =>
+                new ActivityCard(a)).toList(),
               ),
             ),
-          ],
+            ).toList(),
+          ),
         ),
-        _buildUserDialog(),
       ],
+    );
+  }
+
+  Widget _buildUserDialog() {
+    if (_shownUser == null) {
+      return new Container();
+    }
+    return new Center(
+      child: new Container(
+        width: 300.0,
+        height: 200.0,
+        child: new UserProfile(_shownUser, opacity: 0.9),
+      ),
     );
   }
 
@@ -187,6 +143,11 @@ class _ActivitiesScheduleState extends State<ActivitiesSchedulePage>
     _nfcService.NfcStream.asBroadcastStream().listen((id) { _setCurrentAttendee(context, id); });
     return _isLoading
         ? new LoadingSpinner()
-        : _buildPage(context);
+        : new Stack(
+            children: <Widget>[
+              _buildActivitiesList(),
+              _buildUserDialog(),
+            ],
+    );
   }
 }
