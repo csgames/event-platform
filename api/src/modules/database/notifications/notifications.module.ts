b4fccfb9fdc3942669c04dea27a17847664390ee
JulienDuf <dufresne_julien@hotmail.ca>
2018-07-28 18:42:54 -0400

    Cleanup Notifications

--- api/src/modules/database/notifications/notifications.module.ts
+++ api/src/modules/database/notifications/notifications.module.ts
@@ -1,18 +1,25 @@
 import { Module } from "@nestjs/common";
-import { NotificationsController } from "./notifications.controller";
-import { NotificationsProviders } from "./notifications.providers";
-import { NotificationsService } from "./notifications.service";
-import { DatabaseModule } from "../database.module";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AttendeesModule } from "../attendees/attendees.module";
+import { EventsSchema } from "../events/events.model";
+import { NotificationsController } from "./notifications.controller";
 import { NotificationGateway } from "./notifications.gateway";
+import { NotificationsService } from "./notifications.service";
 
 @Module({
-    modules: [AttendeesModule, DatabaseModule],
-    controllers: [NotificationsController],
+    modules: [
+        MongooseModule.forFeature([{
+            name: "notifications",
+            schema: EventsSchema
+        }]),
+        AttendeesModule
+    ],
+    controllers: [
+        NotificationsController
+    ],
     components: [
         NotificationsService,
-        NotificationGateway,
-        ...NotificationsProviders
+        NotificationGateway
     ],
     exports: [
         NotificationsService
