6c33232e4791dec9adc22aa183dfbbd3618218b4
JulienDuf <dufresne_julien@hotmail.ca>
2018-07-28 18:51:22 -0400

    Cleanup Imports

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,22 +1,12 @@
 import { Module } from "@nestjs/common";
-import { AttendeesModule } from "./database/attendees/attendees.module";
-import { EventsModule } from "./database/events/events.module";
-import { RegistrationModule } from "./registration/registration.module";
-import { SchoolsModule } from "./database/schools/schools.module";
-import { TeamsModule } from "./database/teams/teams.module";
 import { MailGateway } from "../gateways/mail.gateway";
-import { ActivitiesModule } from "./database/activities/activities.module";
-import { NotificationsModule } from "./database/notifications/notifications.module";
+import { DatabaseModule } from "./database/database.module";
+import { RegistrationModule } from "./registration/registration.module";
 
 @Module({
     imports: [
-        ActivitiesModule,
-        AttendeesModule,
-        EventsModule,
-        NotificationsModule,
+        DatabaseModule,
         RegistrationModule,
-        SchoolsModule,
-        TeamsModule
     ],
     providers: [
         MailGateway
