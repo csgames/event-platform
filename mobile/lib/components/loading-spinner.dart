be4b91dad533fc4fe2c35a8db51cdfa121a04bde
Laurent Caron <laurentcarondufour@gmail.com>
2018-11-02 16:35:29 -0400

    Added logo to loading spinner (#10)
    * Added logo to loading spinner
    
    * Added Spinkit

--- mobile/lib/components/loading-spinner.dart
+++ mobile/lib/components/loading-spinner.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingSpinner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[CircularProgressIndicator()])
-        ]);
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SpinKitCircle(
+                  color: Colors.grey,
+                  size: 70.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    'assets/logo.png',
+                    width: 120.0
+                  )
+                )
+              ]
+            )
+          ]
+        )
+      ]
+    );
   }
 }
