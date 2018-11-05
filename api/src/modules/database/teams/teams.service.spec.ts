7a4fcd3f1356ba4c220707d7fd683462848bd25e
JulienDuf <dufresne_julien@hotmail.ca>
2018-11-04 23:51:15 -0500

    Test leave function

--- api/src/modules/database/teams/teams.service.spec.ts
+++ api/src/modules/database/teams/teams.service.spec.ts
@@ -23,8 +23,9 @@ class FakeTeams implements Teams {
         Object.assign(this, team);
     }
 
-    public static findOne(condition: { name: string, attendees: string }): FakeDocumentQuery<FakeTeams> {
-        if (condition.name === 'This team exist' || condition.attendees === '5bde6ec00000000000000000') {
+    public static findOne(condition: { name: string, attendees: string, _id: string }): FakeDocumentQuery<FakeTeams> {
+        if (condition.name === 'This team exist' || condition.attendees === '5bde6ec00000000000000000'
+            || condition._id === '5bde6ec00000000000000001') {
             return new FakeDocumentQuery(new FakeTeams({
                 _id: "5bde6ec00000000000000001",
                 name: 'This team exist',
@@ -32,8 +33,20 @@ class FakeTeams implements Teams {
                 event: '',
                 present: false
             } as Teams));
+        } else if (condition._id === '5bde6ec00000000000000002') {
+            return new FakeDocumentQuery(new FakeTeams({
+                _id: "5bde6ec00000000000000001",
+                name: 'This team exist with more than one member',
+                attendees: ['5bde6ec00000000000000000', '5bde6ec00000000000000001'],
+                event: '',
+                present: false
+            } as Teams));
         }
         return new FakeDocumentQuery(null);
+    }
+
+    public static remove(condition: any): FakeDocumentQuery<void> {
+        return new FakeDocumentQuery<void>(null);
     }
 
     // @ts-ignore
@@ -150,6 +163,107 @@ describe('TeamsService', () => {
                 }, '5bde6ec00000000000000000');
                 expect(team._id).to.equal('5bde6ec00000000000000001');
                 expect(team.attendees.length).to.be.equal(2);
+            } catch (e) {
+                fail('Should not throw any exceptions');
+            }
+        });
+    });
+
+    describe("leave", () => {
+        it('Should throw a ATTENDEE_NOT_FOUND CodeException if the attendee does\'t exist', async () => {
+            attendeesService.reset();
+            attendeesService.setup(x => x.findOne(It.isAny())).returns(() => Promise.resolve(null));
+
+            try {
+                await teamsService.leave({
+                    attendeeId: '5bde6ec00000000000000001',
+                    teamId: '5bde6ec00000000000000000'
+                });
+            } catch (e) {
+                expect(e).to.be.instanceOf(CodeException);
+                expect(e.code).to.be.equal(Code.ATTENDEE_NOT_FOUND);
+            }
+        });
+
+        it('Should throw a TEAM_NOT_FOUND CodeException if the team doesn\'t exist', async () => {
+            attendeesService.reset();
+            attendeesService.setup(x => x.findOne(It.isAny())).returns(() => Promise.resolve({
+                _id: '5bde6ec00000000000000001',
+                userId: '5bde6ec00000000000000000'
+            } as Attendees));
+
+            try {
+                await teamsService.leave({
+                    attendeeId: '5bde6ec00000000000000001',
+                    teamId: '5bde6ec00000000000000000'
+                });
+            } catch (e) {
+                expect(e).to.be.instanceOf(CodeException);
+                expect(e.code).to.be.equal(Code.TEAM_NOT_FOUND);
+            }
+        });
+
+        it('Should throw a ATTENDEE_NOT_IN_TEAM CodeException if the attendee isn\'t in the team', async () => {
+            attendeesService.reset();
+            attendeesService.setup(x => x.findOne(It.isAny())).returns(() => Promise.resolve({
+                _id: '5bde6ec00000000000000001',
+                userId: '5bde6ec00000000000000000'
+            } as Attendees));
+
+            try {
+                await teamsService.leave({
+                    attendeeId: '5bde6ec00000000000000001',
+                    teamId: '5bde6ec00000000000000001'
+                });
+            } catch (e) {
+                expect(e).to.be.instanceOf(CodeException);
+                expect(e.code).to.be.equal(Code.ATTENDEE_NOT_IN_TEAM);
+            }
+        });
+
+        it('Should delete the team if the attendee is the only member of the team', async () => {
+            attendeesService.reset();
+            attendeesService.setup(x => x.findOne(It.isAny())).returns(() => Promise.resolve({
+                _id: '5bde6ec00000000000000001',
+                userId: '5bde6ec00000000000000000'
+            } as Attendees));
+            eventsService.reset();
+            eventsService.setup(x => x.findById(It.isAny())).returns(() => Promise.resolve({
+                type: 'A type'
+            } as Events));
+
+            try {
+                const res = await teamsService.leave({
+                    attendeeId: '5bde6ec00000000000000000',
+                    teamId: '5bde6ec00000000000000001'
+                });
+                expect(res.deleted).to.be.true;
+                expect(res.team).to.be.null;
+            } catch (e) {
+                fail('Should not throw any exceptions');
+            }
+        });
+
+        it('Should not delete the team if the attendee isn\'t the only member of the team', async () => {
+            attendeesService.reset();
+            attendeesService.setup(x => x.findOne(It.isAny())).returns(() => Promise.resolve({
+                _id: '5bde6ec00000000000000001',
+                userId: '5bde6ec00000000000000000'
+            } as Attendees));
+            eventsService.reset();
+            eventsService.setup(x => x.findById(It.isAny())).returns(() => Promise.resolve({
+                type: 'A type'
+            } as Events));
+
+            try {
+                const res = await teamsService.leave({
+                    attendeeId: '5bde6ec00000000000000000',
+                    teamId: '5bde6ec00000000000000002'
+                });
+                expect(res.deleted).to.be.false;
+                expect(res.team).to.not.be.null;
+                expect(res.team.attendees.length).to.equal(1);
+                expect(res.team.attendees[0]).to.not.equal('5bde6ec00000000000000000');
             } catch (e) {
                 fail('Should not throw any exceptions');
             }
