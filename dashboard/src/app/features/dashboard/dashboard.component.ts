cae421dd71151e30c49325da2a7bce02c833e519
Brandon Roberge <brandonroberge@hotmail.com>
2018-12-27 11:47:54 -0500

    Improve buttons

--- dashboard/src/app/features/dashboard/dashboard.component.ts
+++ dashboard/src/app/features/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class DashboardComponent implements OnInit {
+
     constructor() {
     }
 
--- dashboard/src/app/features/dashboard/dashboard.module.ts
+++ dashboard/src/app/features/dashboard/dashboard.module.ts
@@ -6,13 +6,15 @@ import { RouterModule } from "@angular/router";
 import { EventsBarComponent } from "./components/events-bar/events-bar.component";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 import { TopNavComponent } from "./components/top-nav/top-nav.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-        RouterModule
+        RouterModule,
+        FlexLayoutModule
     ],
     declarations: [
         DashboardComponent,
