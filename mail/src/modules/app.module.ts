46b26b1860facb72f97e0dffbb54c46ce503e3c6
JulienDuf <dufresne_julien@hotmail.ca>
2019-01-24 11:56:34 -0500

    Added InfoModule

--- mail/src/modules/app.module.ts
+++ mail/src/modules/app.module.ts
@@ -4,11 +4,15 @@ import { DatabaseModule } from "./database/database.module";
 import { WebHookModule } from "./webhook/webhook.module";
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard, JwtModule } from 'nestjs-jwt2';
+import { InfoModule } from './info/info.module';
+import { ConfigModule } from './configs/config.module';
 
 const mongoUri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_ADDRESS}`;
 
 @Module({
     imports: [
+        ConfigModule,
+        InfoModule,
         MongooseModule.forRoot(mongoUri, { useMongoClient: true }),
         DatabaseModule,
         WebHookModule,
