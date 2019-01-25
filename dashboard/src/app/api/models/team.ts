9a5321e96e25fa86bea44f548c63e2db205fad36
Brandon Roberge <brandonroberge@hotmail.ca>
2019-01-25 11:14:08 -0500

    Team model

---
+++ dashboard/src/app/api/models/team.ts
@@ -0,0 +1,10 @@
+import { Attendee } from "./attendee";
+
+export interface Team {
+    name: string;
+    attendees: Attendee[];
+    event: string;
+    school: string;
+    present: boolean;
+    maxMembersNumber: number;
+}
