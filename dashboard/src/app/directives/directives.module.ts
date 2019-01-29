bc239e2cfa6000fc459bad6ebbf28ea439014459
Brandon Roberge <brandonroberge@hotmail.ca>
2019-01-29 14:50:27 -0500

    Auto stash before merge of "master" and "origin/master"

--- dashboard/src/app/directives/directives.module.ts
+++ dashboard/src/app/directives/directives.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { IfRoleDirective } from "./if-role.directive";
 import { IfNotRoleDirective } from "./if-not-role.directive";
+import { ControlStatusDirective } from "./control-status.directive";
 
 @NgModule({
-    declarations: [IfRoleDirective, IfNotRoleDirective],
-    exports: [IfRoleDirective, IfNotRoleDirective]
+    declarations: [IfRoleDirective, IfNotRoleDirective, ControlStatusDirective],
+    exports: [IfRoleDirective, IfNotRoleDirective, ControlStatusDirective]
 })
 export class DirectivesModule {}
