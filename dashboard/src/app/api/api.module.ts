29397f6dfc30838cd86a509998f1a6aa432cc50d
Laurent Caron <laurentcarondufour@gmail.com>
2019-01-29 10:18:49 -0500

    Fixed merge conflicts

--- dashboard/src/app/api/api.module.ts
+++ dashboard/src/app/api/api.module.ts
@@ -6,11 +6,8 @@ import { AttendeeApi } from "./attendee.api";
 import { EventApi } from "./event.api";
 import { EventInterceptor } from "./interceptors/event.interceptor";
 import { RegistrationApi } from "./registration.api";
-<<<<<<< HEAD
 import { ForgetApi } from "./forget.api";
-=======
 import { TeamApi } from "./team.api";
->>>>>>> 977c6c877c236aea3c35c63456cc64a26c481ed1
 
 @NgModule({
     imports: [
@@ -22,11 +19,8 @@ import { TeamApi } from "./team.api";
         AttendeeApi,
         EventApi,
         RegistrationApi,
-<<<<<<< HEAD
         ForgetApi,
-=======
         TeamApi,
->>>>>>> 977c6c877c236aea3c35c63456cc64a26c481ed1
         ApiService
     ]
 })
