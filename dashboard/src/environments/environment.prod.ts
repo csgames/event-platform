c3378b66f9a319f6106c2dbbd387576b3bab4f2a
JulienDuf <dufresne_julien@hotmail.ca>
2019-02-04 10:09:58 -0500

    Added Docker config

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  CSGAMES_API_URL: "https://api.csgames.org/v1/api",
+  GATEWAY_URL: "https://api.csgames.org",
+  IDENTITY_URL: "https://api.csgames.org/v1/identity"
 };
