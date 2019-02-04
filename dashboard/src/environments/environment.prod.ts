01b16c95a53281389233163352ca578cdc467c1e
JulienDuf <dufresne_julien@hotmail.ca>
2019-02-04 11:10:26 -0500

    Fix Docker container start

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  CSGAMES_API_URL: "https://api.csgames.org/v1/api",
+  CSGAMES_API_URL: "https://api.csgames.org/v1/management",
   GATEWAY_URL: "https://api.csgames.org",
   IDENTITY_URL: "https://api.csgames.org/v1/identity"
 };
