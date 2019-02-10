145c5df29f6309c45cabda1e4e9b3d39220f9d84
Julien Dufresne <dufresne_julien@hotmail.ca>
2019-02-10 00:39:18 -0500

    Update environment.ts

--- dashboard/src/environments/environment.ts
+++ dashboard/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    CSGAMES_API_URL: "http://localhost:8080",
+    CSGAMES_API_URL: "http://localhost:8000/v1/api",
     GATEWAY_URL: "http://localhost:8000",
     IDENTITY_URL: "http://localhost:8000/v1/identity"
 };
