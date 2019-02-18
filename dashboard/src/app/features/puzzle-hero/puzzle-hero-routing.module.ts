d366fb8372c1c2e75f2f272d69eef67c027b7143
Brandon Roberge <brandonroberge@hotmail.ca>
2019-02-18 12:31:03 -0500

    Routing

--- dashboard/src/app/features/puzzle-hero/puzzle-hero-routing.module.ts
+++ dashboard/src/app/features/puzzle-hero/puzzle-hero-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-    { path: "**", redirectTo: "tracks" },
-    { path: "tracks", loadChildren: "src/app/features/puzzle-hero/tracks/tracks.module#TracksModule" }
+    { path: "tracks", loadChildren: "src/app/features/puzzle-hero/tracks/tracks.module#TracksModule" },
+    { path: "scoreboard", loadChildren: "src/app/features/puzzle-hero/scoreboard/scoreboard.module#ScoreboardModule" },
+    { path: "**", redirectTo: "tracks" }
 ];
 
 @NgModule({
---
+++ dashboard/src/app/features/puzzle-hero/scoreboard/scoreboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "scoreboard",
+    templateUrl: "scoreboard.template.html"
+})
+export class ScoreboardComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
