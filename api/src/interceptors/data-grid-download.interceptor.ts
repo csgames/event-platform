e922feae46ea16c1a444bef285421e46b433b8d6
Julien Dufresne <julien@digitalmomentum.io>
2019-03-09 15:29:30 -0500

    Added missing file

---
+++ api/src/interceptors/data-grid-download.interceptor.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import { ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class DataGridDownloadInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, call$: Observable<Buffer | string>): Observable<void> {
+        const type = context.switchToHttp().getRequest<express.Request>().query.type;
+        const res = context.switchToHttp().getResponse<express.Response>();
+        return call$.pipe(
+            map(value => {
+                if (type === "xlsx") {
+                    res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                    res.setHeader("Content-Length", value.length);
+                } else if (type === "csv") {
+                    res.setHeader("Content-Type", "text/csv");
+                    res.setHeader("Content-Length", value.length);
+                }
+
+                res.send(value);
+            })
+        );
+    }
+}
