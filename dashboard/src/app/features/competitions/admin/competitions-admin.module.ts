39462e335ead76c4f7a86437642a2a4cd88435aa
JulienDuf <dufresne_julien@hotmail.ca>
2019-03-14 10:50:24 -0400

    Added acitivities actions

--- dashboard/src/app/features/competitions/admin/competitions-admin.module.ts
+++ dashboard/src/app/features/competitions/admin/competitions-admin.module.ts
@@ -6,6 +6,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TranslateModule } from "@ngx-translate/core";
 import { CompetitionsAdminRoutingModule } from "./competitions-admin-routing.module";
 import { CompetitionFormModule } from "./components/competition-form/competition-form.module";
+import { StoreModule } from "@ngrx/store";
+import * as fromCompetitionAdmin from "./store/competition-admin.reducer";
+import { CompetitionAdminEffects } from "./store/competition-admin.effects";
+import { EffectsModule } from "@ngrx/effects";
 
 @NgModule({
     imports: [
@@ -14,6 +18,8 @@ import { CompetitionFormModule } from "./components/competition-form/competition
         ReactiveFormsModule,
         TranslateModule,
         CompetitionFormModule,
+        StoreModule.forFeature("competitionAdmin", fromCompetitionAdmin.reducer),
+        EffectsModule.forFeature([CompetitionAdminEffects]),
 
         CompetitionsAdminRoutingModule
     ],
--- dashboard/src/app/features/competitions/admin/store/competition-admin.actions.ts
+++ dashboard/src/app/features/competitions/admin/store/competition-admin.actions.ts
@@ -1,13 +1,27 @@
 import { Action } from "@ngrx/store";
+import { Activity } from "../../../../api/models/activity";
 
 export enum CompetitionsAdminActionTypes {
     LoadCompetitionsAdmin = "[Competition admin] Load competitions admin",
     CompetitionsAdminLoaded = "[Competition admin] Competitions admin loaded",
-    CompetitionsAdminError = "[Competition admin] Competitions admin error"
+    CompetitionsAdminError = "[Competition admin] Competitions admin error",
+    LoadActivities = "[Competition admin] Load activities",
+    ActivitiesLoaded = "[Competition admin] Activities loaded"
 }
 
 export class LoadCompetitionsAdmin implements Action {
-  readonly type = CompetitionsAdminActionTypes.LoadCompetitionsAdmin;
+    readonly type = CompetitionsAdminActionTypes.LoadCompetitionsAdmin;
+}
+
+export class LoadActivities implements Action {
+    readonly type = CompetitionsAdminActionTypes.LoadActivities;
+}
+
+export class ActivitiesLoaded implements Action {
+    readonly type = CompetitionsAdminActionTypes.ActivitiesLoaded;
+
+    constructor(public payload: Activity[]) {
+    }
 }
 
 export type CompetitionAdminActions = LoadCompetitionsAdmin;
