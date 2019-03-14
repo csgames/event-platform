57456b6c78d4bbb51136d73bbbb93863aafa1faa
Stephanie Leclerc <stephanie.leclerc@polymtl.ca>
2019-03-14 11:29:55 -0400

    Update of effects and reducer for competitions admin

--- dashboard/src/app/features/competitions/admin/store/competition-admin.effects.ts
+++ dashboard/src/app/features/competitions/admin/store/competition-admin.effects.ts
@@ -1,7 +1,25 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { CompetitionsService } from "src/app/providers/competitions.service";
+import { Store } from "@ngrx/store";
+import { State } from "../store/competition-admin.reducer";
+import { LoadCompetitionsAdmin, CompetitionsAdminActionTypes, CompetitionsAdminLoaded } from "./competition-admin.actions";
+import { switchMap, map } from "rxjs/operators";
+import { Competition } from "src/app/api/models/competition";
 
 @Injectable()
 export class CompetitionAdminEffects {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions,
+              private competitionService: CompetitionsService,
+              private store$: Store<State>) { }
+
+  @Effect()
+  loadCompetitions$ = this.actions$.pipe(
+    ofType<LoadCompetitionsAdmin>(CompetitionsAdminActionTypes.LoadCompetitionsAdmin),
+    switchMap(() => this.competitionService.getCompetitionsForEvent()
+            .pipe(
+                map((competitions: Competition[]) => new CompetitionsAdminLoaded(competitions))
+            )
+        )
+  );
 }
--- dashboard/src/app/features/competitions/admin/store/competition-admin.reducer.ts
+++ dashboard/src/app/features/competitions/admin/store/competition-admin.reducer.ts
@@ -1,19 +1,53 @@
 import { CompetitionAdminActions, CompetitionsAdminActionTypes } from "./competition-admin.actions";
+import * as fromApp from "src/app/store/app.reducers";
+import { Competition } from "src/app/api/models/competition";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 
-export interface State {
-
+export interface CompetitionsAdminState {
+  competitions: Competition[];
+  loading: boolean;
+  error: boolean;
 }
 
-export const initialState: State = {
+export interface State extends fromApp.State {
+  competitionsAdmin: CompetitionsAdminState;
+}
 
+export const initialState: CompetitionsAdminState = {
+  competitions: [],
+  loading: false,
+  error: false
 };
 
-export function reducer(state = initialState, action: CompetitionAdminActions): State {
+export function reducer(state = initialState, action: CompetitionAdminActions): CompetitionsAdminState {
   switch (action.type) {
     case CompetitionsAdminActionTypes.LoadCompetitionsAdmin:
-      return state;
+      return {
+        ... state,
+        loading: true
+      };
+    case CompetitionsAdminActionTypes.CompetitionsAdminLoaded:
+      return {
+        ...state,
+        loading: false,
+        competitions: action.competitions
+      };
+    case CompetitionsAdminActionTypes.CompetitionsAdminError:
+      return {
+        ...state,
+        loading: false,
+        competitions: [],
+        error: true
+      };
+
 
     default:
       return state;
   }
 }
+
+export const getCompetitionsAdminState = createFeatureSelector<State, CompetitionsAdminState>("competitionsAdmin");
+
+export const getCompetitionsAdmin = createSelector(getCompetitionsAdminState, (state: CompetitionsAdminState) => state.competitions);
+export const getCompetitionsAdminLoading = createSelector(getCompetitionsAdminState, (state: CompetitionsAdminState) => state.loading);
+export const getCompetitionsAdminError = createSelector(getCompetitionsAdminState, (state: CompetitionsAdminState) => state.error);
