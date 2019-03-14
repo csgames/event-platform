363dcf6078f3480f59cb4af47d8f76bc0c5fdc9d
JulienDuf <dufresne_julien@hotmail.ca>
2019-03-14 13:00:49 -0400

    Added competition add button

--- dashboard/src/app/features/competitions/admin/competitions-admin.component.ts
+++ dashboard/src/app/features/competitions/admin/competitions-admin.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { select, Store } from "@ngrx/store";
 import { getActivities, State } from "./store/competition-admin.reducer";
 import { LoadActivities } from "./store/competition-admin.actions";
+import { CompetitionFormDto } from "./components/competition-form/dto/competition-form.dto";
+import { CompetitionFormComponent } from "./components/competition-form/competition-form.component";
 
 @Component({
     selector: "app-competitions-admin",
@@ -10,11 +12,34 @@ import { LoadActivities } from "./store/competition-admin.actions";
 
 })
 export class CompetitionsAdminComponent implements OnInit {
+    @ViewChild(CompetitionFormComponent)
+    private form: CompetitionFormComponent;
+
     activities$ = this.store$.pipe(select(getActivities));
+
+    public dto = new CompetitionFormDto();
+    public showCreateCompetitionCard = false;
 
     constructor(private store$: Store<State>) { }
 
     public ngOnInit() {
         this.store$.dispatch(new LoadActivities());
+    }
+
+    public clickAddCompetition() {
+        this.showCreateCompetitionCard = true;
+    }
+
+    public onCancelCompetition() {
+        this.dto = new CompetitionFormDto();
+        this.showCreateCompetitionCard = false;
+    }
+
+    public onAdd() {
+        /*if (!this.form.validate()) {
+            return;
+        }
+        this.store$.dispatch(new AddFlashout(this.dto));*/
+        this.onCancelCompetition();
     }
 }
