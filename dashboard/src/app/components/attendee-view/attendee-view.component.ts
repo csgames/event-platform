6220ca71b3e749388b95f7614d1785e875276c71
Laurent Caron <laurentcarondufour@gmail.com>
2019-04-01 12:33:10 -0400

    stuff

--- dashboard/src/app/components/attendee-view/attendee-view.component.ts
+++ dashboard/src/app/components/attendee-view/attendee-view.component.ts
@@ -3,6 +3,8 @@ import { Attendee } from "src/app/api/models/attendee";
 import { Store } from "@ngrx/store";
 import { State } from "src/app/features/team/team-view/store/team-view.reducer";
 import { DeleteAttendee } from "src/app/features/team/team-view/store/team-view.actions";
+import swal from 'sweetalert';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
     selector: "app-attendee-view",
@@ -14,12 +16,35 @@ export class AttendeeViewComponent implements OnInit {
     @Input() attendee: Attendee;
     @Input() showRegister: boolean;
 
-    constructor(private store$: Store<State>) { }
+    constructor(private store$: Store<State>,
+                private translateService: TranslateService) { }
 
     ngOnInit() {
     }
 
     clickDelete() {
-        this.store$.dispatch(new DeleteAttendee(this.attendee._id));
+        const title = this.translateService.instant("components.delete_attendee.sure");
+        const text = this.translateService.instant("components.delete_attendee.deleted");
+        const buttons = [
+            this.translateService.instant("general.button.cancel"),
+            this.translateService.instant("general.button.delete")
+        ];
+        swal({
+            title,
+            text,
+            icon: "warning",
+            buttons,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal(this.translateService.instant("components.delete_attendee.delete"), {
+                icon: "success",
+              });
+              this.store$.dispatch(new DeleteAttendee(this.attendee._id));
+            } else {
+              swal(this.translateService.instant("components.delete_attendee.safe"));
+            }
+        });
     }
 }
