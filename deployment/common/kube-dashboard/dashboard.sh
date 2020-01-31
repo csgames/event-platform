a3ff882704715b6db6a6cb2eed9748e22b0db9a1
JulienDuf <julien@recursyve.io>
2020-01-31 11:59:42 -0500

    Added keel config

--- deployment/common/kube-dashboard/dashboard.sh
+++ deployment/common/kube-dashboard/dashboard.sh
@@ -1,3 +1,3 @@
-echo http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
+echo http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/
 
 kubectl proxy
